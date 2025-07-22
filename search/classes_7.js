4418bc55df7191a1512248fc166af0f1afa3d828
circleci <>
2025-07-22 12:02:29 +0000

    Update documentation

--- search/classes_7.js
+++ search/classes_7.js
@@ -1,7 +1,9 @@
 var searchData=
 [
   ['hash_3c_20xlnt_3a_3acell_5freference_20_3e',['hash&lt; xlnt::cell_reference &gt;',['../structstd_1_1hash_3_01xlnt_1_1cell__reference_01_4.html',1,'std']]],
+  ['hash_3c_20xlnt_3a_3acolor_20_3e',['hash&lt; xlnt::color &gt;',['../structstd_1_1hash_3_01xlnt_1_1color_01_4.html',1,'std']]],
   ['hash_3c_20xlnt_3a_3acolumn_5ft_20_3e',['hash&lt; xlnt::column_t &gt;',['../structstd_1_1hash_3_01xlnt_1_1column__t_01_4.html',1,'std']]],
+  ['hash_3c_20xlnt_3a_3afont_20_3e',['hash&lt; xlnt::font &gt;',['../structstd_1_1hash_3_01xlnt_1_1font_01_4.html',1,'std']]],
   ['hash_3c_20xlnt_3a_3apath_20_3e',['hash&lt; xlnt::path &gt;',['../structstd_1_1hash_3_01xlnt_1_1path_01_4.html',1,'std']]],
   ['header_5ffooter',['header_footer',['../classxlnt_1_1header__footer.html',1,'xlnt']]],
   ['hyperlink',['hyperlink',['../classxlnt_1_1hyperlink.html',1,'xlnt']]]
