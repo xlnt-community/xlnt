xlnt-community/xlnt

commit 114ca8512ee3a463a3e41749836d145c62621b7d

Fix garbage collection of formats (#115)
`format_impl_ptr` is now used instead of a `format_impl*`. All reference
counting is delegated to format_impl_ptr.

`format_impl_ptr` triggers garbage collection when reference count drops
to zero. Previously, garbage collection was run too often, resulting in
a significant overhead when reading many formats (see benchmark-styles).

`format_impl` is owned by the stylesheet iff `format_impl::stylesheet
!== nullptr` If the format is still referenced while the stylesheet is
cleared/destructed, ownership is delegated to `format_impl_ptr` by
setting `format_impl::stylesheet = nullptr`. This may occur when a
`xlnt::format` object still exists when the workbook is cleared.

`format_impl_ptr` implements a custom smart pointer, instead of using
the traditional `std::shared_ptr` / `std::weak_ptr` approach, to allow
better control over the garbage collection process. Garbage collection
should not run when parsing an xlsx file, even not after the document is
fully read, to support a successful round trip, preserving even unused
formats. See the many `round_trip_matches_rw` tests.

`xlnt::format`: return a reference to this, instead of creating
unnecessary copies. The intention seemed to be to create a fluent
interface, but this requires a reference to be returned, as the
`xlnt::format` copy constructor performs a deep (lazy) copy. It's not a
wrapper like e.g. `xlnt::cell.`

`xlnt::detail::references`: implements reference counting for an object.
Copy constructor does not copy the count to the new object, as it is a
new object requiring its own reference counting.

`xlnt::workbook::empty`: `create_format(true)` returns a copy of the
created format. Modifying the returned format, doesn't actually change
the default format itself. Previously this was the case, due to quirks
in the reference counting mechanism.

Fixes #93

include/xlnt/styles/format.hpp
@@ -27,6 +27,7 @@
 #include <string>
 
 #include <xlnt/xlnt_config.hpp>
+#include <detail/implementations/format_impl.hpp>
 
 namespace xlnt {
 
@@ -67,7 +68,7 @@ public:
     /// to true, determines whether the alignment should be enabled for cells using
     /// this format.
     /// </summary>
-    format alignment(const xlnt::alignment &new_alignment, xlnt::optional<bool> applied = {});
+    format& alignment(const xlnt::alignment &new_alignment, xlnt::optional<bool> applied = {});
 
     /// <summary>
     /// Returns true if the alignment of this format should be applied to cells
@@ -85,7 +86,7 @@ public:
     /// to true, determines whether the border should be enabled for cells using
     /// this format.
     /// </summary>
-    format border(const xlnt::border &new_border, xlnt::optional<bool> applied = {});
+    format& border(const xlnt::border &new_border, xlnt::optional<bool> applied = {});
 
     /// <summary>
     /// Returns true if the border set for this format should be applied to cells using the format.
@@ -102,7 +103,7 @@ public:
     /// to true, determines whether the border should be enabled for cells using
     /// this format.
     /// </summary>
-    format fill(const xlnt::fill &new_fill, xlnt::optional<bool> applied = {});
+    format& fill(const xlnt::fill &new_fill, xlnt::optional<bool> applied = {});
 
     /// <summary>
     /// Returns true if the fill set for this format should be applied to cells using the format.
@@ -119,7 +120,7 @@ public:
     /// to true, determines whether the font should be enabled for cells using
     /// this format.
     /// </summary>
-    format font(const xlnt::font &new_font, xlnt::optional<bool> applied = {});
+    format& font(const xlnt::font &new_font, xlnt::optional<bool> applied = {});
 
     /// <summary>
     /// Returns true if the font set for this format should be applied to cells using the format.
@@ -136,7 +137,7 @@ public:
     /// to true, determines whether the number format should be enabled for cells using
     /// this format.
     /// </summary>
-    format number_format(const xlnt::number_format &new_number_format, xlnt::optional<bool> applied = {});
+    format& number_format(const xlnt::number_format &new_number_format, xlnt::optional<bool> applied = {});
 
     /// <summary>
     /// Returns true if the number_format set for this format should be applied to cells using the format.
@@ -158,7 +159,7 @@ public:
     /// to true, determines whether the protection should be enabled for cells using
     /// this format.
     /// </summary>
-    format protection(const xlnt::protection &new_protection, xlnt::optional<bool> applied = {});
+    format& protection(const xlnt::protection &new_protection, xlnt::optional<bool> applied = {});
 
     /// <summary>
     /// Returns true if the pivot table interface is enabled for this format.
@@ -196,12 +197,12 @@ public:
     /// <summary>
     /// Sets the style of this format to a style with the given name.
     /// </summary>
-    format style(const std::string &name);
+    format& style(const std::string &name);
 
     /// <summary>
     /// Sets the style of this format to new_style.
     /// </summary>
-    format style(const class style &new_style);
+    format& style(const class style &new_style);
 
     /// <summary>
     /// Returns the style of this format. If it has no style, an invalid_parameter
@@ -224,12 +225,12 @@ private:
     /// <summary>
     /// Constructs a format from an impl pointer.
     /// </summary>
-    format(detail::format_impl *d);
+    format(detail::format_impl_ptr d);
 
     /// <summary>
     /// The internal implementation of this format
     /// </summary>
-    detail::format_impl *d_ = nullptr;
+    detail::format_impl_ptr d_;
 };
 
 } // namespace xlnt

commit 1c22596ef26e19dc11f118004cad38becd6e57c7

Prevent including source directory from include directory
Move format_impl_ptr to the include directory as it is needed by format.hpp

tests/CMakeLists.txt: If XLNT_SKIP_INTERNAL_TESTS is defined,
only add the public directory to our include path,
to ensure we do not include private files from inside our public header files.

xml_helper.hpp: moved to internal subdirectory to make it clear when we are using internal files.

format_test_suite: moved testing of internal API to format_impl_test_suite.

Fixes regression introduced by PR #115.

Fixes #118

include/xlnt/internal/format_impl_ptr.hpp
@@ -1,0 +1,79 @@
+// Copyright (c) 2025 xlnt-community
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE
+//
+// @license: http://www.opensource.org/licenses/mit-license.php
+// @author: see AUTHORS file
+
+#pragma once
+
+namespace xlnt {
+namespace detail {
+
+class format_impl;
+class XLNT_API format_impl_ptr
+{
+public:
+  format_impl_ptr() = default;
+  format_impl_ptr(const format_impl_ptr& r) : format_(r.format_) {increment();}
+  format_impl_ptr(format_impl_ptr&& r) : format_(r.format_) {r.format_ = nullptr;}
+  format_impl_ptr(format_impl *format) : format_(format) {increment();}
+  ~format_impl_ptr() {decrement();}
+
+  format_impl_ptr& operator=(const format_impl_ptr& r)
+  {
+    if (this == &r)
+      return *this;
+
+    decrement();
+    format_ = r.format_;
+    increment();
+    return *this;
+  }
+
+  format_impl_ptr& operator=(format_impl_ptr&& r)
+  {
+    decrement();
+    format_ = r.format_;
+    r.format_ = nullptr;
+    return *this;
+  }
+
+  std::size_t use_count () const;
+
+  bool is_set () const {return format_ != nullptr;}
+  void clear () {operator=(nullptr);}
+
+  format_impl *get() const {return format_;}
+  format_impl *operator->() const {return get();}
+  operator format_impl *() const {return get();}
+
+  bool operator== (const format_impl_ptr& r) const {return format_ == r.format_;}
+  bool operator== (format_impl *format) const {return format_ == format;}
+
+protected:
+  void increment();
+  void decrement();
+
+protected:
+  format_impl *format_ = nullptr;
+};
+
+} // namespace detail
+} // namespace xlnt

include/xlnt/styles/format.hpp
@@ -27,7 +27,7 @@
 #include <string>
 
 #include <xlnt/xlnt_config.hpp>
-#include <detail/implementations/format_impl.hpp>
+#include <xlnt/internal/format_impl_ptr.hpp>
 
 namespace xlnt {
 

source/detail/implementations/format_impl.hpp
@@ -5,6 +5,7 @@
 
 #include <detail/xlnt_config_impl.hpp>
 
+#include <xlnt/internal/format_impl_ptr.hpp>
 #include <xlnt/styles/alignment.hpp>
 #include <xlnt/styles/border.hpp>
 #include <xlnt/styles/fill.hpp>
@@ -97,54 +98,6 @@ struct format_impl
 private:
     class references references;
     friend class format_impl_ptr;
-};
-
-class XLNT_API format_impl_ptr
-{
-public:
-    format_impl_ptr() = default;
-    format_impl_ptr(const format_impl_ptr& r) : format_(r.format_) {increment();}
-    format_impl_ptr(format_impl_ptr&& r) : format_(r.format_) {r.format_ = nullptr;}
-    format_impl_ptr(format_impl *format) : format_(format) {increment();}
-    ~format_impl_ptr() {decrement();}
-
-    format_impl_ptr& operator=(const format_impl_ptr& r)
-    {
-        if (this == &r)
-            return *this;
-
-        decrement();
-        format_ = r.format_;
-        increment();
-        return *this;
-    }
-
-    format_impl_ptr& operator=(format_impl_ptr&& r)
-    {
-        decrement();
-        format_ = r.format_;
-        r.format_ = nullptr;
-        return *this;
-    }
-
-    std::size_t use_count () const {return format_->references;}
-
-    bool is_set () const {return format_ != nullptr;}
-    void clear () {operator=(nullptr);}
-
-    format_impl *get() const {return format_;}
-    format_impl *operator->() const {return get();}
-    operator format_impl *() const {return get();}
-
-    bool operator== (const format_impl_ptr& r) const {return format_ == r.format_;}
-    bool operator== (format_impl *format) const {return format_ == format;}
-
-protected:
-    void increment();
-    void decrement();
-
-protected:
-    format_impl *format_ = nullptr;
 };
 
 class format_impl_list_item

source/styles/format.cpp
@@ -245,4 +245,9 @@ void detail::format_impl_ptr::decrement()
     }
 }
 
+std::size_t detail::format_impl_ptr::use_count () const
+{
+  return format_->references;
+}
+
 } // namespace xlnt

tests/CMakeLists.txt
@@ -47,7 +47,10 @@ endif()
 file(GLOB HELPERS_HEADERS ${CMAKE_CURRENT_SOURCE_DIR}/helpers/*.hpp)
 file(GLOB HELPERS_SOURCES ${CMAKE_CURRENT_SOURCE_DIR}/helpers/*.cpp)
 
+file(GLOB HELPERS_INTERNAL_HEADERS ${CMAKE_CURRENT_SOURCE_DIR}/helpers/internal/*.hpp)
+
 set(HELPERS ${HELPERS_HEADERS} ${HELPERS_SOURCES})
+set(HELPERS_INTERNAL ${HELPERS_INTERNAL_HEADERS})
 set(RUNNER ${CMAKE_CURRENT_SOURCE_DIR}/runner.cpp)
 
 if(COVERAGE)
@@ -55,14 +58,17 @@ if(COVERAGE)
   set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fprofile-arcs -ftest-coverage")
 endif()
 
-add_executable(xlnt.test ${RUNNER} ${TESTS} ${HELPERS} $<TARGET_OBJECTS:libstudxml>)
+add_executable(xlnt.test ${RUNNER} ${TESTS} ${HELPERS} ${HELPERS_INTERNAL} $<TARGET_OBJECTS:libstudxml>)
 target_link_libraries(xlnt.test PRIVATE xlnt)
 target_include_directories(xlnt.test
-  PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}
-  PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/../source
-  PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/../third-party/libstudxml
-  PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/../third-party/miniz
-  PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/../third-party/fast_float/include)
+  PRIVATE ${CMAKE_CURRENT_SOURCE_DIR})
+if (NOT XLNT_SKIP_INTERNAL_TESTS)
+  target_include_directories(xlnt.test
+    PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/../source
+    PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/../third-party/libstudxml
+    PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/../third-party/miniz
+    PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/../third-party/fast_float/include)
+endif()
 
 set(XLNT_TEST_DATA_DIR ${CMAKE_CURRENT_SOURCE_DIR}/data)
 target_compile_definitions(xlnt.test PRIVATE XLNT_TEST_DATA_DIR="${XLNT_TEST_DATA_DIR}")
@@ -86,6 +92,7 @@ if(MSVC)
 endif()
 
 source_group(helpers FILES ${HELPERS})
+source_group(helpers\\internal FILES ${HELPERS_INTERNAl})
 source_group(runner FILES ${RUNNER})
 source_group(tests\\cell FILES ${CELL_TESTS})
 source_group(tests\\detail FILES ${DETAIL_TESTS})

tests/helpers/path_helper.hpp
@@ -5,8 +5,35 @@
 
 #include <xlnt/utils/exceptions.hpp>
 #include <xlnt/utils/path.hpp>
-#include <detail/utils/string_helpers.hpp>
 #include <xlnt/internal/features.hpp>
+
+#define XLNT_TEST_U8STRING_LITERAL(a) u8##a
+
+#ifdef __cpp_char8_t
+/// Casts const char8_t arrays from C++20 to const char arrays.
+inline const char * to_char_ptr(const char8_t *utf8)
+{
+  return reinterpret_cast<const char *>(utf8);
+}
+
+/// Casts char8_t arrays from C++20 to char arrays.
+inline char * to_char_ptr(char8_t *utf8)
+{
+  return reinterpret_cast<char *>(utf8);
+}
+#endif
+
+// Casts a UTF-8 string literal to a narrow string literal without changing its encoding.
+#ifdef __cpp_char8_t
+// For C++20 and newer, interpret as UTF-8 and then cast to string literal
+#define XLNT_TEST_U8_TO_CHAR_PTR(a) to_char_ptr(a)
+#else
+// For C++11, C++14 and C++17, simply interpret as UTF-8, which works with classic string literals.
+#define XLNT_TEST_U8_TO_CHAR_PTR(a) a
+#endif
+
+// The following weird cast ensures that the string is UTF-8 encoded at all costs!
+#define XLNT_TEST_ENSURE_UTF8_LITERAL(a) XLNT_TEST_U8_TO_CHAR_PTR(XLNT_TEST_U8STRING_LITERAL(a))
 
 #if XLNT_HAS_INCLUDE(<string_view>) && XLNT_HAS_FEATURE(U8_STRING_VIEW)
   #include <string_view>
@@ -29,7 +56,7 @@ class path_helper
 public:
     static xlnt::path test_data_directory()
     {
-        static const std::string data_dir = ENSURE_UTF8_LITERAL(XLNT_TEST_DATA_DIR);
+        static const std::string data_dir = XLNT_TEST_ENSURE_UTF8_LITERAL(XLNT_TEST_DATA_DIR);
         return xlnt::path(data_dir);
     }
 
@@ -47,7 +74,7 @@ public:
 
     static xlnt::path benchmark_data_directory()
     {
-        static const std::string data_dir = ENSURE_UTF8_LITERAL(XLNT_BENCHMARK_DATA_DIR);
+        static const std::string data_dir = XLNT_TEST_ENSURE_UTF8_LITERAL(XLNT_BENCHMARK_DATA_DIR);
         return xlnt::path(data_dir);
     }
 
@@ -65,7 +92,7 @@ public:
 
     static xlnt::path sample_data_directory()
     {
-        static const std::string data_dir = ENSURE_UTF8_LITERAL(XLNT_SAMPLE_DATA_DIR);
+        static const std::string data_dir = XLNT_TEST_ENSURE_UTF8_LITERAL(XLNT_SAMPLE_DATA_DIR);
         return xlnt::path(data_dir);
     }
 

tests/helpers/temporary_file.hpp
@@ -3,7 +3,6 @@
 #include <cstdio>
 #include <string>
 
-#include <detail/external/include_windows.hpp>
 #include <helpers/path_helper.hpp>
 
 namespace {

tests/helpers/test_suite.hpp
@@ -10,7 +10,6 @@
 //#include <helpers/temporary_directory.hpp>
 //#include <helpers/temporary_file.hpp>
 #include <helpers/timing.hpp>
-#include <helpers/xml_helper.hpp>
 
 struct test_status
 {

tests/internal/format_impl_test_suite.cpp
@@ -1,0 +1,122 @@
+// Copyright (c) 2025 xlnt-community
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE
+//
+// @license: http://www.opensource.org/licenses/mit-license.php
+// @author: see AUTHORS file
+
+#include <detail/implementations/format_impl.hpp>
+#include <detail/implementations/stylesheet.hpp>
+#include <helpers/test_suite.hpp>
+#include <xlnt/cell/cell.hpp>
+#include <xlnt/internal/format_impl_ptr.hpp>
+#include <xlnt/styles/fill.hpp>
+#include <xlnt/styles/format.hpp>
+#include <xlnt/workbook/workbook.hpp>
+#include <xlnt/worksheet/worksheet.hpp>
+
+
+class format_impl_test_suite : public test_suite
+{
+  public:
+    format_impl_test_suite()
+    {
+        register_test(test_format_impl_ptr);
+        register_test(test_inplace_editing_non_shared_format);
+        register_test(test_reference);
+    }
+
+    void test_format_impl_ptr()
+    {
+        xlnt::detail::format_impl_ptr p1(new xlnt::detail::format_impl());
+        xlnt_assert_equals(p1.use_count(), 1);
+
+        // copy constructor
+        auto p2 = p1;
+        xlnt_assert_equals(p1.use_count(), 2);
+
+        p2 = nullptr;
+        xlnt_assert_equals(p1.use_count(), 1);
+
+        // self assignment
+        p1 = p1;
+        xlnt_assert_equals(p1.use_count(), 1);
+
+        {
+            // constructor
+            xlnt::detail::format_impl_ptr p4(p1.get());
+            xlnt_assert_equals(p1.use_count(), 2);
+        }
+        // destructor
+        xlnt_assert_equals(p1.use_count(), 1);
+
+        // move constructor
+        auto p3 = std::move(p1);
+        xlnt_assert_equals(p3.use_count(), 1);
+    }
+
+    void test_inplace_editing_non_shared_format()
+    {
+        xlnt::detail::stylesheet s;
+        s.garbage_collection_enabled = false;
+
+        xlnt_assert_equals(s.format_impls.size(), 0);
+
+        xlnt::format f = [&s](const xlnt::format& f){
+            xlnt_assert_equals(s.format_impls.size(), 1);
+            return f;
+        } (s.create_format(false)
+            .border(xlnt::border())
+            .fill(xlnt::fill(xlnt::pattern_fill()))
+            .font(xlnt::font())
+            .number_format(xlnt::number_format())
+            .style("X")
+          );
+
+        xlnt_assert_equals(s.format_impls.size(), 1);
+    }
+
+    void test_reference()
+    {
+        xlnt::detail::references ref1;
+
+        xlnt_assert_equals(ref1, 0);
+
+        ++ref1;
+        xlnt_assert_equals(ref1, 1);
+
+        xlnt::detail::references ref2(ref1);
+        xlnt_assert_equals(ref2, 0); // a copied object is a new object. The new object is not referenced (although it contains the same data as another object that may be referenced)
+
+        xlnt::detail::references ref3;
+        ref3 = ref2; // same, but for copy constructor;
+        xlnt_assert_equals(ref3, 0);
+
+        xlnt::detail::references ref4(std::move(ref1));
+        xlnt_assert_equals(ref4, 0); // also moved objects are new objects.
+        ++ref4;
+        xlnt_assert_equals(ref4, 1);
+
+        xlnt::detail::references ref5;
+        xlnt_assert_equals(ref5, 0);
+        ref5 = std::move(ref4);
+        xlnt_assert_equals(ref5, 0); // idem for move operator
+    }
+};
+static format_impl_test_suite x;

tests/internal/helper_test_suite.cpp
@@ -25,7 +25,7 @@
 #include <stdexcept>
 
 #include <helpers/test_suite.hpp>
-#include <helpers/xml_helper.hpp>
+#include <helpers/internal/xml_helper.hpp>
 #include <helpers/assertions.hpp>
 
 #include <xlnt/utils/exceptions.hpp>

tests/internal/serialization_test_suite.cpp
@@ -27,7 +27,7 @@
 #include <helpers/path_helper.hpp>
 #include <helpers/temporary_file.hpp>
 #include <helpers/test_suite.hpp>
-#include <helpers/xml_helper.hpp>
+#include <helpers/internal/xml_helper.hpp>
 #include <detail/serialization/xlsx_consumer.hpp>
 #include <detail/utils/string_helpers.hpp>
 #include <xlnt/internal/features.hpp>

tests/styles/fill_test_suite.cpp
@@ -27,6 +27,7 @@
 #include <xlnt/cell/cell.hpp>
 #include <xlnt/styles/fill.hpp>
 #include <xlnt/utils/date.hpp>
+#include <xlnt/workbook/workbook.hpp>
 #include <xlnt/worksheet/worksheet.hpp>
 
 class fill_test_suite : public test_suite

tests/styles/format_test_suite.cpp
@@ -21,7 +21,6 @@
 // @license: http://www.opensource.org/licenses/mit-license.php
 // @author: see AUTHORS file
 
-#include <detail/implementations/stylesheet.hpp>
 #include <helpers/test_suite.hpp>
 #include <xlnt/cell/cell.hpp>
 #include <xlnt/styles/fill.hpp>
@@ -35,10 +34,7 @@ class format_test_suite : public test_suite
     format_test_suite()
     {
         register_test(test_issue93);
-        register_test(test_format_impl_ptr);
         register_test(test_format_garbage_collection);
-        register_test(test_inplace_editing_non_shared_format);
-        register_test(test_reference);
     }
 
     void test_issue93()
@@ -70,35 +66,6 @@ class format_test_suite : public test_suite
         xlnt_assert(ws.cell("A1").has_format());
         xlnt_assert(ws.cell("A1").format().fill_applied());
         xlnt_assert_equals(ws.cell("A1").format().fill(), xlnt::gradient_fill());
-    }
-
-    void test_format_impl_ptr()
-    {
-        xlnt::detail::format_impl_ptr p1(new xlnt::detail::format_impl());
-        xlnt_assert_equals(p1.use_count(), 1);
-
-        // copy constructor
-        auto p2 = p1;
-        xlnt_assert_equals(p1.use_count(), 2);
-
-        p2 = nullptr;
-        xlnt_assert_equals(p1.use_count(), 1);
-
-        // self assignment
-        p1 = p1;
-        xlnt_assert_equals(p1.use_count(), 1);
-
-        {
-            // constructor
-            xlnt::detail::format_impl_ptr p4(p1.get());
-            xlnt_assert_equals(p1.use_count(), 2);
-        }
-        // destructor
-        xlnt_assert_equals(p1.use_count(), 1);
-
-        // move constructor
-        auto p3 = std::move(p1);
-        xlnt_assert_equals(p3.use_count(), 1);
     }
 
     void test_format_garbage_collection()
@@ -143,54 +110,6 @@ class format_test_suite : public test_suite
             xlnt_assert_equals(wb.format_count(), 2); // format is still referenced by local variable f.
         }
         xlnt_assert_equals(wb.format_count(), 1);
-    }
-
-    void test_inplace_editing_non_shared_format()
-    {
-        xlnt::detail::stylesheet s;
-        s.garbage_collection_enabled = false;
-
-        xlnt_assert_equals(s.format_impls.size(), 0);
-
-        xlnt::format f = [&s](const xlnt::format& f){
-            xlnt_assert_equals(s.format_impls.size(), 1);
-            return f;
-        } (s.create_format(false)
-            .border(xlnt::border())
-            .fill(xlnt::fill(xlnt::pattern_fill()))
-            .font(xlnt::font())
-            .number_format(xlnt::number_format())
-            .style("X")
-          );
-
-        xlnt_assert_equals(s.format_impls.size(), 1);
-    }
-
-    void test_reference()
-    {
-        xlnt::detail::references ref1;
-
-        xlnt_assert_equals(ref1, 0);
-
-        ++ref1;
-        xlnt_assert_equals(ref1, 1);
-
-        xlnt::detail::references ref2(ref1);
-        xlnt_assert_equals(ref2, 0); // a copied object is a new object. The new object is not referenced (although it contains the same data as another object that may be referenced)
-
-        xlnt::detail::references ref3;
-        ref3 = ref2; // same, but for copy constructor;
-        xlnt_assert_equals(ref3, 0);
-
-        xlnt::detail::references ref4(std::move(ref1));
-        xlnt_assert_equals(ref4, 0); // also moved objects are new objects.
-        ++ref4;
-        xlnt_assert_equals(ref4, 1);
-
-        xlnt::detail::references ref5;
-        xlnt_assert_equals(ref5, 0);
-        ref5 = std::move(ref4);
-        xlnt_assert_equals(ref5, 0); // idem for move operator
     }
 };
 static format_test_suite x;

tests/utils/path_test_suite.cpp
@@ -23,7 +23,7 @@
 // @author: see AUTHORS file
 
 #include <xlnt/utils/path.hpp>
-#include <detail/utils/string_helpers.hpp>
+#include <xlnt/internal/features.hpp>
 #include <helpers/path_helper.hpp>
 #include <helpers/temporary_file.hpp>
 #include <helpers/test_suite.hpp>
@@ -79,7 +79,7 @@ public:
     {
         xlnt::path path(u8"🤔🥳😇");
         path = path.append(u8"🍕🍟🍔");
-        xlnt_assert_equals(path.string(), U8_TO_CHAR_PTR(u8"🤔🥳😇/🍕🍟🍔"));
+        xlnt_assert_equals(path.string(), XLNT_TEST_U8_TO_CHAR_PTR(u8"🤔🥳😇/🍕🍟🍔"));
     }
 #endif
 

tests/workbook/workbook_test_suite.cpp
@@ -26,11 +26,9 @@
 #include <iostream>
 
 #include <xlnt/xlnt.hpp>
-#include <detail/serialization/open_stream.hpp>
 #include <xlnt/utils/exceptions.hpp>
 #include <helpers/temporary_file.hpp>
 #include <helpers/test_suite.hpp>
-#include <detail/constants.hpp>
 
 #include <xlnt/cell/cell.hpp>
 #include <xlnt/styles/format.hpp>

tests/worksheet/range_test_suite.cpp
@@ -29,7 +29,6 @@
 #include <xlnt/worksheet/header_footer.hpp>
 #include <xlnt/worksheet/range.hpp>
 #include <xlnt/worksheet/worksheet.hpp>
-#include <detail/constants.hpp>
 
 class range_test_suite : public test_suite
 {