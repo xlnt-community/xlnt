c9bf17c21018437b2dc428609cac67f301ab9903
circleci <>
2025-11-09 11:23:48 +0000

    Update documentation

--- search/classes_5.js
+++ search/classes_5.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['fill',['fill',['../classxlnt_1_1fill.html',1,'xlnt']]],
   ['font',['font',['../classxlnt_1_1font.html',1,'xlnt']]],
-  ['format',['format',['../classxlnt_1_1format.html',1,'xlnt']]]
+  ['format',['format',['../classxlnt_1_1format.html',1,'xlnt']]],
+  ['format_5fimpl_5fptr',['format_impl_ptr',['../classxlnt_1_1detail_1_1format__impl__ptr.html',1,'xlnt::detail']]]
 ];
