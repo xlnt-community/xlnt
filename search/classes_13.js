51da1e3f7cbf256d8b2647d1c95806705ab98891
circleci <>
2026-02-08 20:31:46 +0000

    Update documentation

--- search/classes_13.js
+++ search/classes_13.js
@@ -1,4 +1,7 @@
 var searchData=
 [
+  ['value_5fwith_5fdefault_5ftype',['value_with_default_type',['../classxlnt_1_1detail_1_1value__with__default__type.html',1,'xlnt::detail']]],
+  ['value_5fwith_5fdefault_5ftype_3c_200_20_3e',['value_with_default_type&lt; 0 &gt;',['../classxlnt_1_1detail_1_1value__with__default__type.html',1,'xlnt::detail']]],
+  ['value_5fwith_5fdefault_5ftype_3c_20unsigned_20int_2c_20100_20_3e',['value_with_default_type&lt; unsigned int, 100 &gt;',['../classxlnt_1_1detail_1_1value__with__default__type.html',1,'xlnt::detail']]],
   ['variant',['variant',['../classxlnt_1_1variant.html',1,'xlnt']]]
 ];
--- search/classes_3.js
+++ search/classes_3.js
@@ -2,5 +2,8 @@ var searchData=
 [
   ['date',['date',['../structxlnt_1_1date.html',1,'xlnt']]],
   ['datetime',['datetime',['../structxlnt_1_1datetime.html',1,'xlnt']]],
+  ['default_5ffp',['default_fp',['../structxlnt_1_1detail_1_1default__fp.html',1,'xlnt::detail']]],
+  ['default_5fnan',['default_nan',['../structxlnt_1_1detail_1_1default__nan.html',1,'xlnt::detail']]],
+  ['default_5fvalue',['default_value',['../structxlnt_1_1detail_1_1default__value.html',1,'xlnt::detail']]],
   ['document_5fsecurity',['document_security',['../classxlnt_1_1document__security.html',1,'xlnt']]]
 ];
