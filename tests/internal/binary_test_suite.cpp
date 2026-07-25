bba576639d584f842711941fec967e4e92742fcd
Laurențiu Leahu-Vlăducu <laurentiu.leahu-vladucu@posteo.at>
2026-07-25 23:46:26 +0200

    Finished implementing binary reader and writer unit tests

--- tests/internal/binary_test_suite.cpp
+++ tests/internal/binary_test_suite.cpp
@@ -67,6 +67,22 @@ class binary_test_suite : public test_suite
         register_test(test_writer_append_vector_valid_int_to_char);
         register_test(test_writer_append_vector_invalid_char_to_int);
         register_test(test_writer_append_vector_invalid_offset);
+        register_test(test_writer_append_reader_empty);
+        register_test(test_writer_append_reader_valid_char_to_char);
+        register_test(test_writer_append_reader_char_to_int);
+        register_test(test_writer_append_reader_valid_int_to_char);
+        register_test(test_writer_append_reader_invalid_char_to_int);
+        register_test(test_writer_append_reader_invalid_writer_offset);
+        register_test(test_writer_append_reader_invalid_reader_offset);
+        register_test(test_read_valid_single_element_from_stream);
+        register_test(test_read_invalid_single_element_from_stream);
+        register_test(test_read_valid_vector_from_stream);
+        register_test(test_read_empty_vector_from_stream);
+        register_test(test_read_invalid_vector_from_stream);
+        register_test(test_read_valid_string_without_NUL_from_stream);
+        register_test(test_read_valid_string_with_NUL_from_stream);
+        register_test(test_read_empty_string_from_stream);
+        register_test(test_read_invalid_string_from_stream);
     }
 
     void test_reader_read_pointer_valid_char_to_char()
@@ -79,7 +95,6 @@ class binary_test_suite : public test_suite
         xlnt_assert_throws_nothing(ptr = reader.read_pointer<char>());
         xlnt_assert_equals(container.data(), ptr);
         xlnt_assert_equals(reader.offset(), sizeof(char));
-
     }
 
     void test_reader_read_pointer_invalid_char_to_char()
@@ -240,10 +255,11 @@ class binary_test_suite : public test_suite
 
         xlnt_assert_equals(writer.count(), 0);
         xlnt_assert_equals(writer.offset(), 0);
-        xlnt_assert_throws_nothing(writer.write<char>(0));
+        xlnt_assert_throws_nothing(writer.write<char>('h'));
         xlnt_assert_equals(container.size(), sizeof(char));
         xlnt_assert_equals(writer.count(), sizeof(char));
         xlnt_assert_equals(writer.offset(), sizeof(char));
+        xlnt_assert_equals(container.at(0), 'h');
     }
 
     void test_writer_write_valid_int_to_char()
@@ -253,7 +269,7 @@ class binary_test_suite : public test_suite
 
         xlnt_assert_equals(writer.count(), 0);
         xlnt_assert_equals(writer.offset(), 0);
-        xlnt_assert_throws_nothing(writer.write<int>(0));
+        xlnt_assert_throws_nothing(writer.write<int>(1234));
         xlnt_assert_equals(container.size(), sizeof(int));
         xlnt_assert_equals(writer.count(), sizeof(int));
         xlnt_assert_equals(writer.offset(), sizeof(int));
@@ -302,9 +318,11 @@ class binary_test_suite : public test_suite
         xlnt::detail::binary_writer<char> writer(container);
 
         xlnt_assert_equals(writer.count(), 0);
-        xlnt_assert_throws_nothing(writer.assign<char>(std::vector<char>{0, 1}));
+        xlnt_assert_throws_nothing(writer.assign<char>(std::vector<char>{'h', 'i'}));
         xlnt_assert_equals(container.size(), 2);
         xlnt_assert_equals(writer.count(), 2);
+        xlnt_assert_equals(container.at(0), 'h');
+        xlnt_assert_equals(container.at(1), 'i');
     }
 
     void test_writer_assign_vector_char_to_int()
@@ -343,7 +361,7 @@ class binary_test_suite : public test_suite
         xlnt::detail::binary_writer<char> writer(container);
 
         xlnt_assert_equals(writer.count(), 0);
-        xlnt_assert_throws_nothing(writer.assign<int>(std::vector<int>{0}));
+        xlnt_assert_throws_nothing(writer.assign<int>(std::vector<int>{1234}));
         xlnt_assert_equals(container.size(), sizeof(int));
         xlnt_assert_equals(writer.count(), sizeof(int));
     }
@@ -498,11 +516,14 @@ class binary_test_suite : public test_suite
     {
         std::vector<char> container{'h', 'i'};
         xlnt::detail::binary_writer<char> writer(container);
+        writer.offset(2); // don't overwrite current contents (shouldn't happen here anyway)
 
         xlnt_assert_equals(writer.count(), 2);
         xlnt_assert_throws_nothing(writer.append<char>(std::vector<char>{}));
         xlnt_assert_equals(container.size(), 2);
         xlnt_assert_equals(writer.count(), 2);
+        xlnt_assert_equals(container.at(0), 'h');
+        xlnt_assert_equals(container.at(1), 'i');
     }
 
     void test_writer_append_vector_valid_char_to_char()
@@ -512,9 +533,16 @@ class binary_test_suite : public test_suite
         writer.offset(2); // don't overwrite current contents
 
         xlnt_assert_equals(writer.count(), 2);
-        xlnt_assert_throws_nothing(writer.append<char>(std::vector<char>{0, 1}));
-        xlnt_assert_equals(container.size(), 4);
-        xlnt_assert_equals(writer.count(), 4);
+        xlnt_assert_throws_nothing(writer.append<char>(std::vector<char>{' ', 'X', 'L', 'N', 'T'}));
+        xlnt_assert_equals(container.size(), 7);
+        xlnt_assert_equals(writer.count(), 7);
+        xlnt_assert_equals(container.at(0), 'h');
+        xlnt_assert_equals(container.at(1), 'i');
+        xlnt_assert_equals(container.at(2), ' ');
+        xlnt_assert_equals(container.at(3), 'X');
+        xlnt_assert_equals(container.at(4), 'L');
+        xlnt_assert_equals(container.at(5), 'N');
+        xlnt_assert_equals(container.at(6), 'T');
     }
 
     void test_writer_append_vector_char_to_int()
@@ -556,7 +584,7 @@ class binary_test_suite : public test_suite
         writer.offset(2); // don't overwrite current contents
 
         xlnt_assert_equals(writer.count(), 2);
-        xlnt_assert_throws_nothing(writer.append<int>(std::vector<int>{0}));
+        xlnt_assert_throws_nothing(writer.append<int>(std::vector<int>{1234}));
         xlnt_assert_equals(container.size(), sizeof(int) + 2);
         xlnt_assert_equals(writer.count(), sizeof(int) + 2);
     }
@@ -600,6 +628,253 @@ class binary_test_suite : public test_suite
         xlnt_assert_equals(writer.offset(), 3);
         // Cannot append to an invalid offset of binary_writer.
         xlnt_assert_throws(writer.append<char>(std::vector<char>{0}), xlnt::invalid_attribute);
+    }
+
+    void test_writer_append_reader_empty()
+    {
+        std::vector<char> container_read;
+        xlnt::detail::binary_reader<char> reader(container_read);
+
+        std::vector<char> container_write{'h', 'i'};
+        xlnt::detail::binary_writer<char> writer(container_write);
+        writer.offset(2); // don't overwrite current contents (shouldn't happen here anyway)
+
+        xlnt_assert_equals(writer.count(), 2);
+        xlnt_assert_throws_nothing(writer.append<char>(reader, 0));
+        xlnt_assert_equals(container_write.size(), 2);
+        xlnt_assert_equals(writer.count(), 2);
+
+        xlnt_assert_throws(writer.append<char>(reader, 1), xlnt::invalid_parameter);
+        xlnt_assert_equals(container_write.size(), 2);
+        xlnt_assert_equals(writer.count(), 2);
+
+        xlnt_assert_equals(container_write.at(0), 'h');
+        xlnt_assert_equals(container_write.at(1), 'i');
+    }
+
+    void test_writer_append_reader_valid_char_to_char()
+    {
+        std::vector<char> container_read{' ', 'X', 'L', 'N', 'T'};
+        xlnt::detail::binary_reader<char> reader(container_read);
+
+        std::vector<char> container{'h', 'i'};
+        xlnt::detail::binary_writer<char> writer(container);
+        writer.offset(2); // don't overwrite current contents
+
+        xlnt_assert_equals(writer.count(), 2);
+        xlnt_assert_throws_nothing(writer.append<char>(reader, 5));
+        xlnt_assert_equals(container.size(), 7);
+        xlnt_assert_equals(writer.count(), 7);
+        xlnt_assert_equals(container.at(0), 'h');
+        xlnt_assert_equals(container.at(1), 'i');
+        xlnt_assert_equals(container.at(2), ' ');
+        xlnt_assert_equals(container.at(3), 'X');
+        xlnt_assert_equals(container.at(4), 'L');
+        xlnt_assert_equals(container.at(5), 'N');
+        xlnt_assert_equals(container.at(6), 'T');
+    }
+
+    void test_writer_append_reader_char_to_int()
+    {
+        std::vector<int> container_write{0, 1};
+        xlnt::detail::binary_writer<int> writer(container_write);
+        writer.offset(2); // don't overwrite current contents
+
+        xlnt_assert_equals(writer.count(), 2);
+        constexpr std::size_t COUNT = 2;
+        std::vector<char> container_read;
+        container_read.reserve(COUNT * sizeof(int) + 1);
+        xlnt::detail::binary_reader<char> reader(container_read);
+
+        // Cannot append a size that does not contain the exact same
+        // number of bytes as the size of the element.
+        for (std::size_t n = 0; n < COUNT * sizeof(int) + 1; ++n)
+        {
+            if (n % sizeof(int) == 0)
+            {
+                xlnt_assert_throws_nothing(writer.append<char>(reader, n));
+                const std::size_t num_ints = n / sizeof(int);
+                xlnt_assert_equals(container_write.size(), num_ints + 2);
+                xlnt_assert_equals(writer.count(), num_ints + 2);
+                writer.offset(2);
+            }
+            else
+            {
+                xlnt_assert_throws(writer.append<char>(reader, n), xlnt::invalid_parameter);
+            }
+
+            container_read.push_back(n);
+        }
+    }
+
+    void test_writer_append_reader_valid_int_to_char()
+    {
+        std::vector<int> container_read{0};
+        xlnt::detail::binary_reader<int> reader(container_read);
+
+        std::vector<char> container{'h', 'i'};
+        xlnt::detail::binary_writer<char> writer(container);
+        writer.offset(2); // don't overwrite current contents
+
+        xlnt_assert_equals(writer.count(), 2);
+        xlnt_assert_throws_nothing(writer.append<int>(reader, 1));
+        xlnt_assert_equals(container.size(), sizeof(int) + 2);
+        xlnt_assert_equals(writer.count(), sizeof(int) + 2);
+    }
+
+    void test_writer_append_reader_invalid_char_to_int()
+    {
+        std::vector<char> container_read{0};
+        xlnt::detail::binary_reader<char> reader(container_read);
+
+        std::vector<int> container{0, 1};
+        xlnt::detail::binary_writer<int> writer(container);
+        writer.offset(2); // don't overwrite current contents
+
+        xlnt_assert_equals(writer.count(), 2);
+        // Cannot append a size that does not contain the exact same
+        // number of bytes as the size of the element.
+        xlnt_assert_throws(writer.append<char>(reader, 1), xlnt::invalid_parameter);
+        xlnt_assert_equals(container.size(), 2);
+        xlnt_assert_equals(writer.count(), 2);
+    }
+
+    void test_writer_append_reader_invalid_writer_offset()
+    {
+        std::vector<char> container_read{0};
+        xlnt::detail::binary_reader<char> reader(container_read);
+
+        std::vector<char> container{'h', 'i'};
+        xlnt::detail::binary_writer<char> writer(container);
+        writer.offset(2); // don't overwrite current contents
+
+        xlnt_assert_equals(writer.count(), 2);
+        xlnt_assert_equals(writer.offset(), 2);
+        writer.offset(3);
+        xlnt_assert_equals(writer.count(), 2);
+        xlnt_assert_equals(writer.offset(), 3);
+        // Cannot append to an invalid offset of binary_writer.
+        xlnt_assert_throws(writer.append<char>(reader, 1), xlnt::invalid_attribute);
+        xlnt_assert_equals(container.size(), 2);
+        writer.offset(2);
+
+        xlnt_assert_throws_nothing(writer.append<char>(reader, 1));
+        xlnt_assert_equals(container.size(), 3);
+        xlnt_assert_equals(writer.count(), 3);
+        xlnt_assert_equals(writer.offset(), 3);
+        container.pop_back();
+        xlnt_assert_equals(writer.count(), 2);
+        xlnt_assert_equals(writer.offset(), 3);
+        // Cannot append to an invalid offset of binary_writer.
+        xlnt_assert_throws(writer.append<char>(reader, 1), xlnt::invalid_attribute);
+    }
+
+    void test_writer_append_reader_invalid_reader_offset()
+    {
+        std::vector<char> container_read{0};
+        xlnt::detail::binary_reader<char> reader(container_read);
+
+        std::vector<char> container{'h', 'i'};
+        xlnt::detail::binary_writer<char> writer(container);
+        writer.offset(2); // don't overwrite current contents
+
+        xlnt_assert_equals(reader.count(), 1);
+        xlnt_assert_equals(reader.offset(), 0);
+        reader.offset(1);
+        xlnt_assert_equals(reader.count(), 1);
+        xlnt_assert_equals(reader.offset(), 1);
+        // Cannot append from an invalid offset of binary_reader.
+        xlnt_assert_throws(writer.append<char>(reader, 1), xlnt::invalid_parameter);
+        xlnt_assert_equals(container.size(), 2);
+        reader.offset(0);
+        xlnt_assert_equals(reader.offset(), 0);
+        // Cannot append an invalid number of elements of binary_reader.
+        xlnt_assert_throws(writer.append<char>(reader, 2), xlnt::invalid_parameter);
+    }
+
+    void test_read_valid_single_element_from_stream()
+    {
+        std::stringstream ss;
+        ss << 'h';
+        char elem = '\0';
+        xlnt_assert_throws_nothing(elem = xlnt::detail::read<char>(ss));
+        xlnt_assert_equals(elem, 'h');
+    }
+
+    void test_read_invalid_single_element_from_stream()
+    {
+        std::stringstream ss;
+        char elem = '\0';
+        xlnt_assert_throws(elem = xlnt::detail::read<char>(ss), xlnt::invalid_parameter);
+        xlnt_assert_equals(elem, '\0');
+    }
+
+    void test_read_valid_vector_from_stream()
+    {
+        std::stringstream ss;
+        ss << "hi XLNT";
+        std::vector<char> vec;
+        xlnt_assert_throws_nothing(vec = xlnt::detail::read_vector<char>(ss, 7));
+        xlnt_assert_equals(vec.at(0), 'h');
+        xlnt_assert_equals(vec.at(1), 'i');
+        xlnt_assert_equals(vec.at(2), ' ');
+        xlnt_assert_equals(vec.at(3), 'X');
+        xlnt_assert_equals(vec.at(4), 'L');
+        xlnt_assert_equals(vec.at(5), 'N');
+        xlnt_assert_equals(vec.at(6), 'T');
+    }
+
+    void test_read_empty_vector_from_stream()
+    {
+        std::stringstream ss;
+        std::vector<char> vec;
+        xlnt_assert_throws_nothing(vec = xlnt::detail::read_vector<char>(ss, 0));
+    }
+
+    void test_read_invalid_vector_from_stream()
+    {
+        std::stringstream ss;
+        std::vector<char> vec;
+        xlnt_assert_throws(vec = xlnt::detail::read_vector<char>(ss, 1), xlnt::invalid_parameter);
+    }
+
+    void test_read_valid_string_without_NUL_from_stream()
+    {
+        std::stringstream ss;
+        ss << "hi XLNT";
+        std::string str;
+        xlnt_assert_throws_nothing(str = xlnt::detail::read_string<char>(ss, 7, false));
+        xlnt_assert_equals(str, "hi XLNT");
+    }
+
+    void test_read_valid_string_with_NUL_from_stream()
+    {
+        std::stringstream ss;
+        std::string_view str_with_NUL {"hi XLNT\0", 8};
+        ss << str_with_NUL;
+        std::string str;
+        xlnt_assert_throws_nothing(str = xlnt::detail::read_string<char>(ss, 8, true));
+        xlnt_assert_equals(str, "hi XLNT");
+        ss.seekg(0);
+        xlnt_assert_throws_nothing(str = xlnt::detail::read_string<char>(ss, 8, false));
+        xlnt_assert_equals(str, str_with_NUL);
+    }
+
+    void test_read_empty_string_from_stream()
+    {
+        std::stringstream ss;
+        std::string str;
+        // Reading 0 elements must always work.
+        xlnt_assert_throws_nothing(str = xlnt::detail::read_string<char>(ss, 0, true));
+        // Re-reading 0 elements must also work.
+        xlnt_assert_throws_nothing(str = xlnt::detail::read_string<char>(ss, 0, false));
+    }
+
+    void test_read_invalid_string_from_stream()
+    {
+        std::stringstream ss;
+        std::string str;
+        xlnt_assert_throws(str = xlnt::detail::read_string<char>(ss, 1, true), xlnt::invalid_parameter);
     }
 };
 
